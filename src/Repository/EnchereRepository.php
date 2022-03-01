6b663f0f248008dc91ee179a028ab38b05958d31
Kelig <kelig.villalard@gmail.com>
2022-03-01 16:56:20 +0100

    coreection prix reserve

--- src/Repository/EnchereRepository.php
+++ src/Repository/EnchereRepository.php
@@ -37,7 +37,7 @@ class EnchereRepository extends ServiceEntityRepository
                 ->setParameter(':enchereId',$enchereId)
                 ->orderBy('e.datedebut', 'ASC')
                 ->setParameter('ladate',$ladate)
-                ->select("e.id,DATE_FORMAT(e.datedebut,'%Y-%m-%d') AS date_debut,DATE_FORMAT(e.datefin,'%Y-%m-%d') AS date_fin,prixreserve,t.id AS type_enchere_id,p.id AS produit_id")            
+                ->select("e.id,DATE_FORMAT(e.datedebut,'%Y-%m-%d') AS date_debut,DATE_FORMAT(e.datefin,'%Y-%m-%d') AS date_fin,e.prixreserve,t.id AS type_enchere_id,p.id AS produit_id")            
                 ->getQuery()
                 ->getResult()
             ;
@@ -48,7 +48,7 @@ class EnchereRepository extends ServiceEntityRepository
                 ->andWhere('e.datedebut > :ladate')
                 ->orderBy('e.datedebut', 'ASC')
                 ->setParameter('ladate',$ladate)
-                ->select("e.id,DATE_FORMAT(e.datedebut,'%Y-%m-%d') AS date_debut,DATE_FORMAT(e.datefin,'%Y-%m-%d') AS date_fin,prixreserve,t.id AS type_enchere_id,p.id AS produit_id")            
+                ->select("e.id,DATE_FORMAT(e.datedebut,'%Y-%m-%d') AS date_debut,DATE_FORMAT(e.datefin,'%Y-%m-%d') AS date_fin,e.prixreserve,t.id AS type_enchere_id,p.id AS produit_id")            
                 ->getQuery()
                 ->getResult()
             ;
@@ -67,7 +67,7 @@ class EnchereRepository extends ServiceEntityRepository
                 ->setParameter(':enchereId', $enchereId)
                 ->orderBy('e.datedebut', 'ASC')
                 ->setParameter('ladate',$ladate)
-                ->select("e.id,DATE_FORMAT(e.datedebut,'%Y-%m-%d') AS date_debut,DATE_FORMAT(e.datefin,'%Y-%m-%d') AS date_fin,prixreserve,t.id AS type_enchere_id,p.id AS produit_id")            
+                ->select("e.id,DATE_FORMAT(e.datedebut,'%Y-%m-%d') AS date_debut,DATE_FORMAT(e.datefin,'%Y-%m-%d') AS date_fin,e.prixreserve,t.id AS type_enchere_id,p.id AS produit_id")            
                 ->getQuery()
                 ->getResult();
         }else{
@@ -92,7 +92,7 @@ class EnchereRepository extends ServiceEntityRepository
             ->andWhere('u.id = :userID')
             ->orderBy('e.datedebut','ASC')
             ->setParameter(':userID',$user->getId())
-            ->select("e.id,DATE_FORMAT(e.datedebut,'%Y-%m-%d') AS date_debut,DATE_FORMAT(e.datefin,'%Y-%m-%d') AS date_fin,prixreserve,t.id AS type_enchere_id, p.id AS produit_id")
+            ->select("e.id,DATE_FORMAT(e.datedebut,'%Y-%m-%d') AS date_debut,DATE_FORMAT(e.datefin,'%Y-%m-%d') AS date_fin,e.prixreserve,t.id AS type_enchere_id, p.id AS produit_id")
             ->getQuery()
             ->getResult();
             
