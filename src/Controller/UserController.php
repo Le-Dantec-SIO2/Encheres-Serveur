293c014cdc2cd2a2453accb53d9ff80d0d81cae6
Kelig <kelig.villalard@gmail.com>
2022-03-03 11:18:04 +0100

    test n° 844713

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -77,7 +77,7 @@ class UserController extends AbstractController
             $mail = null;
             $pass = null;
         }
-        dump($mail);
+        dd($mail);
         $encoder = new JsonEncoder();
         $defaultContext = [
     AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
