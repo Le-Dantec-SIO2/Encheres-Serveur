335adc346250b7f5a3cda8ab93b0990953f6d288
thierry le gall <thierry28220@live.fr>
2022-03-07 20:40:27 +0100

    $dateNormalizer = new DateTimeNormalizer(array('datetime_format' => 'Y-m-d'))

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -20,6 +20,8 @@ class Utils{
                     },
                 ];
                 $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+                $dateNormalizer = new DateTimeNormalizer(array('datetime_format' => 'Y-m-d'))
+
                 $serializer = new Serializer([$normalizer], [$encoder]);
                 $data = $request->getContent();
                 $data = $serializer->serialize($var, 'json',[AbstractNormalizer::IGNORED_ATTRIBUTES => $ignoredFields]);
