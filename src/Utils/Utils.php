63c8cd51990128a3c5d13eb9c279b8206b062252
thierry le gall <thierry28220@live.fr>
2022-03-07 20:10:16 +0100

    Symfony\Component\Serializer\Normalizer\DateTimeNormalizer

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 
 class Utils{
 
