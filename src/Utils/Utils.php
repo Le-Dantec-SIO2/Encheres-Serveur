54321758aac0bcc7ca0f56c7643c818a5f31ec27
thierry le gall <thierry28220@live.fr>
2022-03-07 20:38:44 +0100

    $dateNormalizer = new DateTimeNormalizer(array('datetime_format' => 'Y-m-d'))

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\Normalizer\DateTimeNormalizer;
 
 class Utils{
 
