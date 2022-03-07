7fa4d484b65fe1d0523a64bc595173598ed0cce5
thierry le gall <thierry28220@live.fr>
2022-03-07 20:50:04 +0100

    H:i:s

--- src/Utils/Utils.php
+++ src/Utils/Utils.php
@@ -20,7 +20,7 @@ class Utils{
                     },
                 ];
                 $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
-                $dateNormalizer = new DateTimeNormalizer(array('datetime_format' => 'Y-m-d'));
+                $dateNormalizer = new DateTimeNormalizer(array('datetime_format' => 'Y-m-d H:i:s'));
 
                 $serializer = new Serializer([$dateNormalizer,$normalizer], [$encoder]);
                 $data = $request->getContent();
