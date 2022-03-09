f7108760f06d642126e60d130f4efe12c1d5575e
Sukando <abignon.ledantec@gmail.com>
2022-03-09 17:51:31 +0100

    Debug removed

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -46,7 +46,6 @@ class UserController extends AbstractController
         } 
         else 
             return  Utils::ErrorMissingArgumentsDebug($request->getContent());
-        return  Utils::ErrorMissingArgumentsDebug($request->getContent());
         $var = $userRepository->findUserByEmailAndPass(['email' => $email],['password' => $password]);
         $response = new Utils;
         return $response->GetJsonResponse($request, $var);
