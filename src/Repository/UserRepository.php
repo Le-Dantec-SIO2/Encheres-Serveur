4a6909c075cc152c34752933a368f00b3b649b58
thierry le gall <thierry28220@live.fr>
2022-03-16 14:33:02 +0100

    user

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -57,7 +57,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     public function findUserById($userId)
     {
         return $this->createQueryBuilder('u')
-            ->select('u.id', 'u.email', 'u.pseudo',CAST(CONVERT('u.photo' using utf8) AS photo) )
+            ->select('u.id', 'u.email', 'u.pseudo',CAST(CONVERT('u.photo' using 'utf8') AS photo) )
             ->andWhere('u.id = :val')
             ->setParameter('val', $userId)
             ->orderBy('u.id', 'ASC')
