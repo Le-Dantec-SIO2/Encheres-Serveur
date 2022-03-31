6dd89bf219be165a5f4582520a9030a667e8dc5a
Sukando <abignon.ledantec@gmail.com>
2022-03-31 11:42:51 +0200

    Debug Postencherir

--- src/Controller/EncherirController.php
+++ src/Controller/EncherirController.php
@@ -42,8 +42,8 @@ class EncherirController extends AbstractController
         //On récupère le prix de l'offre
         $prixoffre = $postdata->PrixEnchere;
         //On récupère le Coefficient envoyer en paramètre si il y'en a un sinon mettre a 1 par défaut
-        isset($postdata->Coefficient) ?  $coefficient =(float) $postdata->Coefficient : $coefficient = 1;
-
+        isset($postdata->Coefficient) ?  $coefficient = floatval($postdata->Coefficient) : $coefficient = 1;
+        
         //On vérifie que le prix proposer est cohérent et valide 
         $authorize = EncherirController::PriceAuthorize($postdata->IdEnchere, $prixoffre, $coefficient, $encherirRepository, $enchereRepository);
         if ($authorize != null)
