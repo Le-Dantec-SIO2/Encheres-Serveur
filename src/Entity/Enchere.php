8cf2b1dfaf3d383d5a3237a7a133ba0ff1af3b8b
thierry le gall <thierry28220@live.fr>
2022-04-08 13:30:00 +0200

    ok

--- src/Entity/Enchere.php
+++ src/Entity/Enchere.php
@@ -264,16 +264,16 @@ class Enchere
 
     public function GetMontantNouvelleEnchere(float $paramEnchere, float $paramTaux)
     {
-      int retour = round( rand(0,$paramTaux) * $paramEnchere);
-      int rnd = round( rand(0,9);
+      int $retour = round( rand(0,$paramTaux) * $paramEnchere);
+      int $rnd = round( rand(0,9);
       if(rnd>4)
       {
-         return   $paramEnchere+retour;
+         $retour=   $paramEnchere+retour;
       }
       else 
       {
-	    return   $paramEnchere-retour;
+	    $retour=   $paramEnchere-retour;
       }
-
+      return $retour;
     }
 }
