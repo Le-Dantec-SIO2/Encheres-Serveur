1952a83bd1845ae89552bd9b1ac47c35a616fcb4
thierry le gall <thierry28220@live.fr>
2022-04-08 12:08:23 +0200

    ok

--- src/Controller/PlayerFlashController.php
+++ src/Controller/PlayerFlashController.php
@@ -96,7 +96,7 @@ class PlayerFlashController extends AbstractController
         $derniereEnchere = $encherirRepository->findActualPrice($postdata->IdEnchere);
 
         //On calcule la valeur de la nouvelle enchère
-        $nouvelleEnchere = $enchere->GetMontantNouvelleEnchere(12,0.08);
+        $nouvelleEnchere = $enchere->GetMontantNouvelleEnchere($derniereEnchere,0.38);
        
         //on renvoie
         //On renvoie une réponse pour savoir si l'opération à réussie
