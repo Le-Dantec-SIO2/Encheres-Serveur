9ca267d14253d3da0c624955b6712f49db495628
Kelig <kelig.villalard@gmail.com>
2022-05-03 16:46:27 +0200

    correction enchères participées

--- src/Controller/EncheresController.php
+++ src/Controller/EncheresController.php
@@ -70,8 +70,7 @@ class EncheresController extends AbstractController
     {
         $postdata = json_decode($request->getContent());
         $userId = isset($postdata->Id) ? $postdata->Id : null;
-        dd(IsNull($userId));
-        $var = IsNull($userId) ? $enchereRepository->findEncheresAll() : $enchereRepository->findEncheresParticipes($userId);
+        $var = $userId == null ? $enchereRepository->findEncheresAll() : $enchereRepository->findEncheresParticipes($userId);
         $response = new Utils;
         return $response->GetJsonResponse($request, $var);
     }
