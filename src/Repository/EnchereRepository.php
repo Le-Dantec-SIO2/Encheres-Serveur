3a99b56d3a453fc5718427c5f25869f5625db54e
Kelig <kelig.villalard@gmail.com>
2022-05-05 10:45:23 +0200

    debug select

--- src/Repository/EnchereRepository.php
+++ src/Repository/EnchereRepository.php
@@ -146,7 +146,7 @@ class EnchereRepository extends ServiceEntityRepository
             ->leftJoin('en.leuser', 'u')
             ->andWhere('u.id = :userID')
             ->orderBy('e.datedebut', 'ASC')
-            ->select("e.id,e.leproduit,e.letypeenchere,e.datedebut,e.datefin,e.prixreserve,e.prixdepart,e.visibilite,e.lemagasin,e.tableau_flash,en.id,en.prixenchere,en.dateenchere,p.id,p.nom,p.photo,p.prixreel")
+            ->select("e.id,e.leproduit,e.letypeenchere,e.datedebut,e.datefin,e.prixreserve,e.prixdepart,e.visibilite,e.lemagasin,e.tableau_flash")
             ->setParameter(':userID', $userId)
             ->groupBy("e.id")
             ->getQuery()
